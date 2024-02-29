095cc801917f195314a92eaa0140dabeed30ef3e
lauramarie99 <lauramarie.pilgram@web.de>
2024-02-29 10:10:21 +0000

    improve readability

--- diffuse.py
+++ diffuse.py
@@ -12,55 +12,43 @@ import yaml
 import argparse
 
 # Run subprocess
-def run(command, trigger, total_timesteps):
-    #progress = widgets.FloatProgress(min=0, max=1, description='running', bar_style='info')
-    #display(progress)
-    pattern = re.compile(f'.*{trigger}.*')
-    #progress_counter = 0
+def run(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
     while True:
         line = process.stdout.readline()
         if not line: break    
-        #if pattern.match(line):
-        #progress_counter += 1
-        #progress.value = progress_counter / total_timesteps
     return_code = process.wait()
-    #progress.description = "done"
 
 # Run diffusion
 def run_diffusion(contigs, name, path,
                   pdb=None, 
                   iterations=50,
-                  symmetry="null", 
                   num_designs=10,
                   guide_scale=1,
                   guide_potentials="",
-                  substrate="",
+                  substrate="2PE",
                   ckpt_override_path="null",
-                  enzyme_design=True,
+                  enzyme_design=False,
                   noise_scale=1,
                   deterministic=False):
     """
     This function runs a diffusion simulation using provided input parameters, 
-    applies symmetry and contigs processing, and generates the final PDB structures.
+    applies contigs processing, and generates the final PDB structures.
 
     Args:
     contigs (str): Input contigs string to define the fixed and free portions.
+    name (str): Experiment name.
     path (str): The output directory path for generated results.
     pdb (str, optional): The PDB file path. Defaults to None.
     iterations (int, optional): Number of diffusion iterations. Defaults to 50.
-    symmetry (str, optional): Symmetry type ("null", "auto", "cyclic", "dihedral"). Defaults to "none".
-    order (int, optional): Order for cyclic and dihedral symmetry. Defaults to 1.
-    hotspot (str, optional): Hotspot residues for the protein-protein interaction site. Defaults to None.
-    chains (str, optional): Chains to include in the simulation. Defaults to None.
-    add_potential (bool, optional): If True, adds oligomer contact guiding potentials. Defaults to False.
-    num_designs (int, optional): Number of designs to generate. Defaults to 5.
-    guide_scale (float): Scaling factor for guiding potentials. Defaults to 20.
+    num_designs (int, optional): Number of designs to generate. Defaults to 10.
+    guide_scale (float): Scaling factor for guiding potentials. Defaults to 1.
     guide_potentials (str): The guiding potentials string. Defaults to an empty string.
     substrate (str): The substrate design. Defaults to "2PE".
-    ckpt_override_path (str): The path of the checkpoint file. Defaults to "./RFdiffusion/models/ActiveSite_ckpt.pt".
-    enzyme_design (bool, optional): If True, performs enzyme design by adding guiding potentials. Defaults to True.
-    noise_scale (int, optional): Change noise_scale_ca and noise_scale_frame
+    ckpt_override_path (str): The path of the checkpoint file. Defaults to "null".
+    enzyme_design (bool, optional): If True, generates substrate pockets by adding guiding potential. Defaults to False.
+    noise_scale (int, optional): Change noise_scale_ca and noise_scale_frame.
+    deterministic (bool, optional): Deterministic initialization.
     
     Returns:
     tuple: The updated contigs list and the number of symmetry-equivalent copies.
@@ -68,7 +56,6 @@ def run_diffusion(contigs, name, path,
 
     # Make output directory
     full_path = f"{path}{name}/Diffusion"
-    print(full_path)
     os.makedirs(full_path, exist_ok=True)
     output_prefix = f"{full_path}/{name}"
 
@@ -77,9 +64,9 @@ def run_diffusion(contigs, name, path,
             f"inference.num_designs={num_designs}",
             f"denoiser.noise_scale_ca={noise_scale}",
             f"denoiser.noise_scale_frame={noise_scale}",
-            f"inference.ckpt_override_path={ckpt_override_path}"]
+            f"inference.ckpt_override_path={ckpt_override_path}",
+            f"diffuser.T={iterations}"]
 
-    # Determine the mode based on the provided contigs input
     contigs = contigs.replace(",", " ").replace(":", " ").split()
     is_fixed, is_free = False, False
     fixed_chains = []
@@ -107,29 +94,19 @@ def run_diffusion(contigs, name, path,
     else:
         mode = "free"
 
-    # Symmetry
-    if symmetry != "null":
-       raise Exception("Symmetry options are not implemented yet!")
-    else:
-       copies = 1
+    copies = 1
     
-    # Process input contigs based on specified mode
+    # Process contigs and options for fixed mode
     if mode == "fixed":
       
         # Get PDB string
         pdb_str = pdb_to_string(pdb, chains=fixed_chains)
-        #print(f"pdb_str: {pdb_str}")
-      
-        # Get input PDB
+        # Store input PDB in outdir
         pdb_filename = f"{full_path}/input.pdb"
         os.system(f"cp {pdb} {pdb_filename}")
-
         # Parse the PDB file and update options
         parsed_pdb = parse_pdb(pdb_filename)
         opts.append(f"inference.input_pdb={pdb_filename}")
-
-        # Process contigs and options for the fixed mode
-        opts.append(f"diffuser.T={iterations}")
         # Print prefix contigs for diagnostic purposes
         print("prefix contigs:", contigs)
         contigs = fix_contigs(contigs, parsed_pdb)
@@ -137,7 +114,6 @@ def run_diffusion(contigs, name, path,
     
     # Process contigs and options for the free mode
     elif mode == "free":
-        opts.append(f"diffuser.T={iterations}")
         parsed_pdb = None
         contigs = fix_contigs(contigs, parsed_pdb)
     
@@ -166,10 +142,8 @@ def run_diffusion(contigs, name, path,
     opts_str = " ".join(opts)
     cmd = f"python3.9 ./RFdiffusion/run_inference.py {opts_str}"
     print(cmd)
-
     # Run the command using a helper function "run"
-    steps = (iterations - 1) * num_designs
-    run(cmd, "Timestep", steps)
+    run(cmd)
 
     # Post-processing: fix PDB structures based on contigs
     for n in range(num_designs):
@@ -188,10 +162,7 @@ def run_diffusion(contigs, name, path,
     return contigs, copies
 
 
-
-
-
-# Get config
+# Read given config
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, required=True)
 args = parser.parse_args()
