c731889d51d92f86aa9901f0f912deb3cee349bb
lauramarie99 <lauramarie.pilgram@web.de>
2024-02-29 12:29:36 +0000

    remove paths

--- run_cluster.py
+++ run_cluster.py
@@ -2,10 +2,10 @@
 import subprocess, glob, time, os, argparse
 
 # Create a slurm script
-def create_slurm_script(slurm_path, container, config, script, outdir, name, jobname, 
-                        time='24:00:00', mem='10000', cpus=1, gpu='a30:1', partition='paula', 
-                        email='zo23uqar@studserv.uni-leipzig.de', emailType='FAIL',
-                        excludeNodes='paula02,paula04', dependency=''):
+def create_slurm_script(colabdesign_path, slurm_path, container, config, script, 
+                        outdir, name, jobname, time='24:00:00', mem='10000', cpus=1, 
+                        gpu='a30:1', partition='paula', email='', emailType='FAIL', 
+                        excludeNodes='', dependency=''):
     with open(f'{slurm_path}/{name}.slurm', 'w') as slurmFile:
         slurmFile.writelines([
                 "#!/bin/bash\n",
@@ -30,148 +30,123 @@ def create_slurm_script(slurm_path, container, config, script, outdir, name, job
                 f'CONTAINER={container}\n',
                 '# define SCRIPT or program to call inside the container\n',
                 f'SCRIPT="{script} --config {config}"\n',
-                'cd /home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/ColabDesign\n',
+                f'cd {colabdesign_path}\n',
                 'singularity exec --nv --cleanenv $CONTAINER $SCRIPT\n'      
         ])
 
-
-# Run single slurm script
+# Run single slurm script, returns job id and error message
 def run_slurm_script(name, cwd):
     slurm_command = f'/usr/bin/sbatch {name}.slurm'
     process = subprocess.Popen(slurm_command,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        universal_newlines=True,
-                        shell=True,
-                        cwd=cwd)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True,
+                               shell=True,
+                               cwd=cwd)
     output, error = process.communicate()
-
     if error == '':
         output = output.split()[-1]
-
     return output, error
 
-
-# Run all slurm scripts
+# Run all slurm scripts, returns dictionary with job ids and dictionary with error messages
 def run_all_slurm_scripts(slurm_path):
     slurm_files = glob.glob(
         f'{slurm_path}/*.slurm')
     job_ids = {}
     errors = {}
-
     for slurm_file in slurm_files:
         name = slurm_file.split('/')[-1].split('.')[0]
-        output, error = run_slurm_script(
-            name=name,
-            cwd=slurm_path)
+        output, error = run_slurm_script(name=name, cwd=slurm_path)
         job_ids[name] = output
         errors[name] = error
-
     return job_ids, errors
 
-
-# Check if job is already done
+# Check if job is already done, returns True/False
 def check_if_job_is_done(job_id):
     time.sleep(1)
     slurm_command = f'/usr/bin/squeue -j {job_id}'
     process = subprocess.Popen(slurm_command,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                universal_newlines=True,
-                                shell=True)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True,
+                               shell=True)
     output, error = process.communicate()
-
     if job_id in output:
         return False
-    else:
-        return True
+    return True
 
-# Check if diffusion was successfull
-def check_if_diffusion_done(name,slurm_path):
-    if not os.path.exists(f"{slurm_path}Diffusion/{name}.out"):
+# Check if diffusion was successfull, returns True/False
+def check_if_diffusion_done(name, slurm_path):
+    if not os.path.exists(f"{slurm_path}/Diffusion/{name}.out"):
         return False
-    with open(f"{slurm_path}Diffusion/{name}.out") as myfile:
+    with open(f"{slurm_path}/Diffusion/{name}.out") as myfile:
         if "the final contigs are" in myfile.read():
             return True
-        else:
-            return False
-
-# Check if validation was successfull
-def check_if_validation_done(name,slurm_path):
-    if not os.path.exists(f"{slurm_path}Validation/{name}.out"):
         return False
-    with open(f"{slurm_path}Validation/{name}.out") as myfile:
+
+# Check if validation was successful, returns True/False
+def check_if_validation_done(name,slurm_path):
+    if not os.path.exists(f"{slurm_path}/Validation/{name}.out"):
+        return False
+    with open(f"{slurm_path}/Validation/{name}.out") as myfile:
         if "running designability" in myfile.read():
             return True
-        else:
-            return False
-
-# Check if GPU is used during diffusion
-def check_if_gpu_used(name,slurm_path):
-    if not os.path.exists(f"{slurm_path}Diffusion/{name}.err"):
         return False
-    with open(f"{slurm_path}Diffusion/{name}.err") as myfile:
+
+# Check if GPU is used during diffusion, returns Ture/False
+def check_if_gpu_used(name, slurm_path):
+    if not os.path.exists(f"{slurm_path}/Diffusion/{name}.err"):
+        return False
+    with open(f"{slurm_path}/Diffusion/{name}.err") as myfile:
         if "CUDA unknown error" in myfile.read():
             return False
-        else:
-            return True
+        return True
 
-# Cancel job
+# Cancel job, returns output and error message
 def cancel_job(job_id):
     slurm_command = f'/usr/bin/scancel {job_id}'
     process = subprocess.Popen(slurm_command,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            universal_newlines=True,
-                            shell=True)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True,
+                               shell=True)
     output, error = process.communicate()
     return output, error
 
-# Resubmit job
+# Resubmit job, returns output and error message
 def resubmit_job(name,job_id,slurm_path):
     cancel_job(job_id)
-    output, error = run_slurm_script(
-            name=name,
-            cwd=slurm_path)
-
+    output, error = run_slurm_script(name=name,cwd=slurm_path)
     return output, error
     
-# Start diffusion jobs     
-def run_diffusion(config_path, slurm_path, container):
+# Start diffusion jobs, returns dictionary with job ids and dictionary with error messages   
+def run_diffusion(colabdesign_path, config_path, slurm_path, container, diffusion_path):
     config_files = glob.glob(
         f'{config_path}/*.yml')
     for config_file in config_files:
         name = config_file.split('/')[-1].split('.')[0]
-        create_slurm_script(slurm_path=slurm_path,container=container,config=config_file,script="python3.9 /home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/ColabDesign/diffuse.py",
-                            outdir=slurm_path,name=f"{name}_diffusion",jobname=f"diff-{name}", time="01:00:00", mem="10000", cpus=1,
-                            gpu="a30:1",partition="paula",email="zo23uqar@studserv.uni-leipzig.de",emailType="FAIL",excludeNodes='paula02,paula04')
+        create_slurm_script(colabdesign_path=colabdesign_path, slurm_path=slurm_path, container=container,
+                            config=config_file, script=diffusion_path, outdir=slurm_path,name=f"{name}_diffusion",
+                            jobname=f"diff-{name}", time="01:00:00", mem="10000", cpus=1, gpu="a30:1", 
+                            partition="paula",email="", emailType="FAIL", excludeNodes='')
     job_ids, errors = run_all_slurm_scripts(slurm_path=slurm_path)
     return job_ids, errors
 
 
-# Start validation job if diffusion is done
-def run_validation(config_path, slurm_path, diffusion_job_ids, container):
+# Start validation job if diffusion is done, returns dictionary with job ids and dictionary with error messages 
+def run_validation(colabdesign_path, config_path, slurm_path, diffusion_job_ids, container, validation_path):
     validation_job_ids = {}
     validation_errors = {}
     for name,job_id in diffusion_job_ids.items():
         counter = 0
         new_job_id = job_id
-        # Check if job is done
-        # while not check_if_job_is_done(job_id=new_job_id):
-        #     time.sleep(15)
-        # Check if diffusion was successfull
         exp_name = name[:-10]
-        # diffusion_done = check_if_diffusion_done(name,slurm_path)
-        # Run validation if diffusion was successful
-        # if diffusion_done:
-            #print("Diffusion done:", exp_name)
-        config_file = f"{config_path}{exp_name}.yml"
+        config_file = f"{config_path}/{exp_name}.yml"
         slurm_name = f"{exp_name}_validation"
-        create_slurm_script(slurm_path=slurm_path,container=container,config=config_file,
-                script="python3 /home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/ColabDesign/validate.py",
-                outdir=slurm_path,name=slurm_name,jobname=f"val-{exp_name}", time="01:00:00", 
-                mem="10000", cpus=1, gpu="a30:1",partition="paula",email="zo23uqar@studserv.uni-leipzig.de",
-                emailType="FAIL",excludeNodes='paula02,paula04', dependency=job_id)
+        create_slurm_script(colabdesign_path=colabdesign_path, slurm_path=slurm_path, container=container,
+                            config=config_file, script=validation_path, outdir=slurm_path, name=slurm_name,
+                            jobname=f"val-{exp_name}", time="01:00:00", mem="10000", cpus=1, gpu="a30:1",
+                            partition="paula", email="", emailType="FAIL", excludeNodes="", dependency=job_id)
     validation_job_ids, errors = run_all_slurm_scripts(slurm_path=slurm_path)
     return validation_job_ids, validation_errors
 
@@ -182,25 +157,38 @@ MAIN
 
 # Global variables
 argParser = argparse.ArgumentParser()
-argParser.add_argument('-r','--run')                                          # name of run
+argParser.add_argument('-r','--run')                                                                    # Name of run
 args = argParser.parse_args()
 
-diffusion_container = "/home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/proteindesign.sif"
-validation_container = "/home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/colabdesign1.1.0.sif"
-config_path = f"/home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/Input/Diffusion/{args.run}/Configs/"
-slurm_path = f"/home/sc.uni-leipzig.de/lu341pjya/ProteinDesign/Input/Diffusion/{args.run}/Slurm/"
-
+# Adapt paths!
+diffusion_container = "/home/proteindesign.sif"                                                         # Location diffusion container
+validation_container = "/home/colabdesign1.1.0.sif"                                                     # Location validation container
+config_path = f"/home/{args.run}/Configs"                                                               # Location config files
+slurm_path = f"/home/{args.run}/Slurm"                                                                  # Location slurm files
+colabdesign_path = "/home/Colabdesign"                                                                  # Location colabdesign repository
+diffusion_path = "python3.9 diffuse.py"                                                                 # Call diffuse.py 
+validation_path = "python3 validate.py"                                                                 # Call validate.py
 
 # Run diffusion and validation
-if not os.path.exists(f"{slurm_path}Diffusion"):
-   os.makedirs(f"{slurm_path}Diffusion")
-   os.makedirs(f"{slurm_path}Validation")
-diffusion_job_ids, diffusion_errors = run_diffusion(config_path=config_path,slurm_path=f"{slurm_path}Diffusion",container=diffusion_container)
+if not os.path.exists(f"{slurm_path}/Diffusion"):
+   os.makedirs(f"{slurm_path}/Diffusion")
+   os.makedirs(f"{slurm_path}/Validation")
+diffusion_job_ids, diffusion_errors = run_diffusion(colabdesign_path=colabdesign_path, 
+                                                    config_path=config_path,
+                                                    slurm_path=f"{slurm_path}/Diffusion",
+                                                    container=diffusion_container,
+                                                    diffusion_path=diffusion_path)
 print("Diffusion jobs submitted")
-# print("Errors:", diffusion_errors)
-validation_job_ids, validation_errors = run_validation(config_path=config_path,slurm_path=f"{slurm_path}Validation",diffusion_job_ids=diffusion_job_ids,container=validation_container)
+
+validation_job_ids, validation_errors = run_validation(colabdesign_path=colabdesign_path,
+                                                       config_path=config_path,
+                                                       slurm_path=f"{slurm_path}/Validation",
+                                                       diffusion_job_ids=diffusion_job_ids,
+                                                       container=validation_container,
+                                                       validation_path=validation_path)
 print("Validation jobs submitted")
-# print("Errors:", validation_errors)
+
+# Check if validation successful
 for name,job_id in validation_job_ids.items():
     while not check_if_job_is_done(job_id=job_id):
         time.sleep(30)
@@ -209,4 +197,3 @@ for name,job_id in validation_job_ids.items():
         print("Validation done:", exp_name)
     else:
         print("Validation failed:", exp_name)
-    
